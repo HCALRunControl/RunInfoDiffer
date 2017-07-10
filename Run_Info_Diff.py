0bb19f1f6f3280939c5534e5b20528e4682ef464
CiaranGodfrey <ciaran.laji.godfrey@cern.ch>
2017-07-10 16:58:20 +0200

    improve variable names, color output, set runnumber query to most recent run

--- Run_Info_Diff.py
+++ Run_Info_Diff.py
@@ -2,75 +2,91 @@ import cx_Oracle
 import logging
 import time
 import difflib
+ 
+
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+
 
 password = raw_input("database password:")
 database = "cms_hcl_test_runinfo/%s@cms_orcoff_prep" % password
-
-
+ 
+database = "cms_hcl_test_runinfo/%s@cms_orcoff_prep" % password
 connection = cx_Oracle.connect(database)
 cur = connection.cursor()
-
-#tab = cur.execute('SELECT RUNNUMBER FROM RUNNUMBERTBL WHERE rownum=1')
-#tab = cur.execute('SELECT MAX(RUNNUMBER) FROM RUNSESSION_PARAMETER')
-#tables = cur.fetchall()
-#print tables
-
-
+ 
 fields = ['CMS.HCAL_HBHEa:HCAL_CFGSCRIPT','CMS.HCAL_LEVEL_1:HCAL_CFGSCRIPT','CMS.HCAL_LASER:HCAL_CFGSCRIPT','CMS.HCAL_HBHEb:HCAL_CFGSCRIPT','CMS.HCAL_HO:HCAL_CFGSCRIPT','CMS.HCAL_HBHEc:HCAL_CFGSCRIPT','CMS.HCAL_HF:HCAL_CFGSCRIPT','CMS.HCAL_HBHEa:CFGDOC_TXT','CMS.HCAL_HBHEb:CFGDOC_TXT','CMS.HCAL_HBHEc:CFGDOC_TXT','CMS.HCAL_LASER:CFGDOC_TXT','CMS.HCAL_HO:CFGDOC_TXT','CMS.HF:CFGDOC_TXT']
-def get_fields(runum):
-    value = {}
-    for field in fields:
-        try:
-            SQLstatement = 'SELECT value FROM runsession_string  WHERE runsession_parameter_id=(SELECT id FROM runsession_parameter WHERE (runnumber='+str(runum)+' AND name=\''+field+'\'))'
-            r = cur.execute(SQLstatement)
-            result = cur.fetchall()
-            if result != []:
-                result = result[0][0].read()
-            value[field] = result
-        except Exception as e:
-            value[field] = "Field does not exist"
-            print e
-    return value
 
-def diff(old, new):
-    if old==new:
-        return None
+ 
+#parameters_file = open("diffed_parmeters.txt","r")
+#for line in parameters_file:
+    #parse line to get prefixs
+    #loop over prefixes and format parameters
+    #add parameters to fields[]
+
+#parameters_file.close()
+ 
+def get_fields(runum):
+    query_return_values = {}
+    for field in fields:
+         try:
+            SQLstatement = 'SELECT value FROM runsession_string  WHERE runsession_parameter_id=(SELECT id FROM runsession_parameter WHERE (runnumber='+str(runum)+' AND name=\''+field+'\'))'
+            cur.execute(SQLstatement)
+            query_result = cur.fetchall()
+            if query_result != []:
+                query_result = query_result[0][0].read()
+                query_return_values[field] = query_result
+            else:
+                query_return_values[field] = ""
+         except Exception as e:
+            query_return_values[field] = "Field does not exist"
+            print e
+    return query_return_values
+
+def runinfo_differ(old_parameters, new_parameters):
+    if old_parameters==new_parameters:
+        return False
     else:
-        message = ""	
-        for key in old:
-            if old[key]==new[key]:
+        message = ""   
+        for key in old_parameters:
+            if old_parameters[key]==new_parameters[key]:
                 continue
             else:
-                old_string = str(old[key]).strip().splitlines()
-                new_string = str(new[key]).strip().splitlines()
+                old_string = str(old_parameters[key]).strip().splitlines()
+                new_string = str(new_parameters[key]).strip().splitlines()
                 d = difflib.Differ()
-                result = list(d.compare(old_string, new_string))
-                message = key + ' changed:\n'
-                for line in result:
-                    if line[0] == "+" or line[0] == "-":
-                        message += line + '\n'
-        return message
+                changes = list(d.compare(old_string, new_string))
+                if len(changes)!=0:
+                    color_print(key, changes)
+        return True
+
+def color_print(parameter, message):
+    print(parameter + ' changed:\n')
+    for line in message:
+        if line[0] == "+":
+            print(G+line+W)
+        elif line[0] == "-":
+            print(R+line+W)
 
 try:
-    cur.execute('SELECT runnumber FROM RUNSESSION_PARAMETER where runnumber=1000027333')
+    cur.execute('SELECT MAX(RUNNUMBER) FROM RUNSESSION_PARAMETER')
     previous_runnumber = cur.fetchall()[0][0]
-    previous_value = get_fields(previous_runnumber)
+    previous_parameter_values = get_fields(previous_runnumber)
     count = 0
     while count < 5:
         count += 1
         cur.execute('SELECT MAX(RUNNUMBER) FROM RUNSESSION_PARAMETER')
         recent_runnumber = cur.fetchall()[0][0]
         if recent_runnumber != previous_runnumber:
-            new_value = get_fields(recent_runnumber)
-            difference = diff(previous_value, new_value)
-    	    if difference != None:
-                print difference
-                previous_value.clear()
-                previous_value.update(new_value)
+            new_parameter_values = get_fields(recent_runnumber)
+            difference = runinfo_differ(previous_parameter_values, new_parameter_values)
+            if difference:
+                previous_parameter_values.clear()
+                previous_parameter_values.update(new_parameter_values)
         time.sleep(10)
 except Exception as e:
     logging.exception(e)
 
 cur.close()
 connection.close()
-
