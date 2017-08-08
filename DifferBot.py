e6576ca157be13d341abf75d564670e3439f9d5d
Ciaran Laji Godfrey <cgodfrey@srv-s2f17-17-01.cms>
2017-08-08 16:55:22 +0200

    read from remote log file and hide webhook

--- DifferBot.py
+++ DifferBot.py
@@ -13,40 +13,37 @@ class webDifferParser(HTMLParser):
          HTMLParser.__init__(self)
          self.data = []
     def handle_data(self, data):
-         if not data.isspace() and "Runinfo Diff" not in data:
+         if not data.isspace() and "Runinfo Diff" not in data and "Run Info Differ Log":
               self.data.append(data.strip())
     def clear_diff(self):
          self.data = []
 
 parser = webDifferParser()
-
+log_url = "http://hcalmon.cms/RunInfoDiffer/BotUrlLog.html"
 def read_log():
-    Diff_urls = open("BotUrlLog.txt","r")
-    lines = Diff_urls.readlines()
-    Diff_urls.close()
-    return lines[-1]
+    log = getoutput('curl -s -k --socks5-hostname localhost:1080 \"'+log_url+'\"')
+    parser.clear_diff()
+    #print log
+    for line in log.splitlines():
+	parser.feed(line)
+    return parser.data[-2]+"&"+parser.data[-1]
 
 def get_diff(url):
-    diff = getoutput('curl -s \"'+url+'\"')
+    diff = getoutput('curl -s -k --socks5-hostname localhost:1080 \"'+url+'\"')
     parser.clear_diff()
     for line in diff.splitlines():
 	parser.feed(line)
 
 def send_slack_message(runnumber1, runnumber2, message):
-    webhook_url = 'https://hooks.slack.com/services/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'
+    webhook_url = 'WEBHOOK'
     slack_data = {'text': "Run parameters changed between run %d and run %d ```%s```" % (runnumber1, runnumber2, message)}
-    print str(runnumber1)+str(runnumber2)
-    print message
     response = requests.post(
       webhook_url, data=json.dumps(slack_data),
       headers={'Content-Type': 'application/json'}
     )
     if response.status_code != 200:
         print 'Request to slack returned an error %s, the response is:\n%s' % (response.status_code._codes[response.status_code][0], response.text)
-        return False
-    else:
-	print "all good"
-        return True
+
 
 def is_int(string):
     try:
@@ -56,7 +53,7 @@ def is_int(string):
 	return False
 
 previous_url = read_log()
-for i in range(0,10):
+while True:
     new_url = read_log()
     if new_url != previous_url:
 	get_diff(new_url)
@@ -72,7 +69,7 @@ for i in range(0,10):
 		message += line+"\n"
 	send_slack_message(runnumbers[0], runnumbers[1], message)
 	#print message
-    sleep(10)
+    sleep(20)
 
 
 
